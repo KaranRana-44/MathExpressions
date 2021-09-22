370ea18a852eb00a41f914fc3874dfa9effc6b7e
student <karanrana@bilericak12.com>
2021-09-22 11:23:05 -0400

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	double r = Math.random(); //[0.0, 1.0)
+	System.out.println(r);
+	r= Math.random() * 10;
+	System.out.println(r);
+	int p = (int)(Math.random()*10);
+	System.out.println(p);
+	p = (int)(Math.random()*10)+ 1;
+	System.out.println(p);
+	p = (int)(Math.random()*10)+ 21;
+	System.out.println(p);
     }
 }
